355b9e542c20db75baa8a63d584b072abde91932
XingYu Ye <yexingyu@itiger.com>
2025-05-21 19:52:17 +0800

    本地开发配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api/v1/chat/completions": {
+        target: "https://wings-tigerai-hub.test.tigerbrokers.net/",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
 })
